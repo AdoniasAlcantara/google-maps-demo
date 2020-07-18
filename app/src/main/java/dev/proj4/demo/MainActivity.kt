1bcc6789473f0a1752f6493d4c46e56f0c8c9163
AdoniasAlcantara <adoniasasneto@gmail.com>
2020-07-18 20:29:46 -0300

    Map setup

--- app/src/main/java/dev/proj4/demo/MainActivity.kt
+++ app/src/main/java/dev/proj4/demo/MainActivity.kt
@@ -1,5 +1,22 @@
 package dev.proj4.demo
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager.findFragmentById(R.id.mapHolder)?.let { fragment ->
+            fragment as SupportMapFragment
+            fragment.getMapAsync(::onMapReady)
+        }
+    }
+
+    private fun onMapReady(googleMap: GoogleMap) {
+        println("Map is ready")
+    }
+}
